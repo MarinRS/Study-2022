f4977612c08e0417ad7b3d448924a15f69a62d57
User <roma_marin@mail.ru>
2022-04-17 13:10:34 +0300

    Lesson 1 (First commit)

---
+++ src/main/java/ru/gb/marinrs/Main.java
@@ -0,0 +1,11 @@
+package ru.gb.marinrs;
+
+public class Main {
+    public static void  main (String [] args){
+        System.out.println ("Hello world!");
+        System.out.println("Привет мир!!");
+        System.out.println("Шалом православные!!!");
+        System.out.println("Мы русские с нами Бог!!!!");
+        System.out.println("С вербным воскресеньем !!!!!");
+    }
+}
